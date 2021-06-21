1f1db8c152bdbb04fd96012b05029de42dc04658
Luca Giozza <luca.giozza@gmail.com>
2021-06-21 16:58:14 +0200

    stampo del messaggio

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Stampare a schermo un messaggio all’interno di un h1,
+//  utilizzando i data.
+
+const app = new Vue (
+
+{
+    el: '#app',
+
+    data : {
+        messaggio : 'Il messaggio è stato stampato'
+        
+    },
+
+
+}
+
+
+)
